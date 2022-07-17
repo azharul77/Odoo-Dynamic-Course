1aa5286c4a471f45ba20627abd6d003c59b2541b
azharul77 <suvromulla921@gmail.com>
2022-07-17 11:43:52 +0600

    first commit

---
+++ __manifest__.py
@@ -0,0 +1,23 @@
+{
+    'name': 'Cutom attendees',
+    'version': '14.0.1.0.0',
+    'summary': 'This module is for custom snippet',
+    'sequence': -102,
+    'description': """The purpose to create this module is generate a custom module in website snippet.""",
+    'category': 'sale and invoicing',
+    'depends': ['base',
+                'web',
+                'website',
+                'website_slides' ],
+    'data': [
+        ],
+    'demo': [],
+    'installable': True,
+    'application': True,
+    'auto-install': False,
+    'license': 'LGPL-3',
+    'contributors': [
+        'Md Azharul Amin Mulla',
+    ],
+
+}
